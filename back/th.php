e736d65e48cf67e877d621cdc7dbf6ccf799a61f
charles100 <brs-68@yahoo.com.tw>
2024-08-12 23:53:27 +0800

    建立後台分類管理頁面

---
+++ back/th.php
@@ -0,0 +1,28 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    <label for="">新增大分類</label>
+    <input type="text" name="big" id="big">
+    <button>新增</button>
+</div>
+<div class="ct">
+    <label for="">新增中分類</label>
+    <select name="bigs" id="bigs"></select>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
+</div>
+<table class="all">
+    <tr class="tt">
+        <td></td>
+        <td class="ct">
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr class="tt">
+        <td></td>
+        <td class="ct">
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+</table>
