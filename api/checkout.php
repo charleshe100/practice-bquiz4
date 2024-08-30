3ae033ff591c5b6e7dd61ad48bb6f6c296ed7011
charles100 <brs-68@yahoo.com.tw>
2024-08-30 23:54:36 +0800

    完成前台購物車結帳及送出訂單功能

---
+++ api/checkout.php
@@ -0,0 +1,16 @@
+<?php include_once "db.php";
+
+//後六碼用亂數來產生
+$_POST['no']=date("Ymd").rand(100000,999999);
+
+$_POST['acc']=$_SESSION['user'];
+
+$_POST['orderdate']=date("Y-m-d");
+
+//將購物實陣列轉為字串
+$_POST['cart']=serialize($_SESSION['cart']);
+
+$Order->save($_POST);
+
+unset($_SESSION['cart']);
+?>
